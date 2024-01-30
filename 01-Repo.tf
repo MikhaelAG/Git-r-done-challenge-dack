1d38a059bd310b4fded3d1da5a43d7a3475736d7
MikhaelAG <130944619+MikhaelAG@users.noreply.github.com>
2024-01-29 21:52:35 -0600

    Update 01-Repo.tf

--- 01-Repo.tf
+++ 01-Repo.tf
@@ -1,20 +1,21 @@
-resource "github_repository" "foo" {
-  name      = "repo-test5-s"
+resource "github_repository" "repo" {
+  name      = "put_name_of_your_repo_here"
   auto_init = true
 }
 
 resource "github_repository_file" "tf_files" {
-  for_each            = fileset("C:/Users/adria/OneDrive/Desktop/terraform_cliv/AWS/saopowpow/ultraman66", "/*.tf")
-  repository          = github_repository.foo.name
+  for_each            = fileset("Copy+paste_full_path_to_your_terraform_folder_here", "/*.tf")
+  repository          = github_repository.repo.name
   branch              = "main"
-  file                = each.value#".gitignore"
-  content             = file("C:/Users/adria/OneDrive/Desktop/terraform_cliv/AWS/saopowpow/ultraman66/${each.key}")
+  file                = each.value
+  content             = file("Copy+paste_full_path_to_your_terraform_folder_here/${each.value}")
   commit_author       = "Terraform User"
   commit_email        = "terraform@example.com"
   overwrite_on_create = true
 }
 
+## To add a branch to your GitHub repo
 resource "github_branch" "branch_name"{
-  repository = github_repository.foo.name
+  repository = github_repository.repo.name
   branch = "development"
 }
