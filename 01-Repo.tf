f139dd1f386a0625b0daf979ccb0ac0ab5c8da3a
MikhaelAG <130944619+MikhaelAG@users.noreply.github.com>
2024-02-19 09:23:15 -0600

    Update 01-Repo.tf

--- 01-Repo.tf
+++ 01-Repo.tf
@@ -1,8 +1,14 @@
+## GitOps for Terraform Repos
+
+## 1. Create GitHub Repo
+##    And Git init (auto)
 resource "github_repository" "repo" {
   name      = "put_name_of_your_repo_here"
   auto_init = true
 }
 
+## 2. Add Terraform files to GitHub repo
+##    Commit and Push files to repo
 resource "github_repository_file" "tf_files" {
   for_each            = fileset("Copy+paste_full_path_to_your_terraform_folder_here", "/*.tf")
   repository          = github_repository.repo.name
@@ -14,7 +20,7 @@ resource "github_repository_file" "tf_files" {
   overwrite_on_create = true
 }
 
-## To add a branch to your GitHub repo
+##3.  Add a branch to your GitHub repo
 resource "github_branch" "branch_name"{
   repository = github_repository.repo.name
   branch = "development"
